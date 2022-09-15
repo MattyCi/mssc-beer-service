54c707cff1b27a33ddc3f11242e730bc2b26dfab
MattyCi <mjciaravino@gmail.com>
2022-09-15 16:56:12 -0400

    initial commit

---
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
